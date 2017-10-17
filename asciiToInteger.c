1c902115657c1fe7a4272cddc9c191f103edaaf3
jee00609 <31675804+jee00609@users.noreply.github.com>
2017-10-17 20:30:36 +0900

    프로그래밍 언어 응용 6번째 과제
    scanf()를 쓰지 않기 위해 문자형 변수에 '0'를 빼서 만들었습니다.
    간혹 깃허브에서 문자출력이 깨져서 나오는 경우가 있습니다.
    너그러운 마음으로 봐주셨으면 좋겠습니다.

---
+++ asciiToInteger.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void asciiToInteger(char *str);
+
+int main ()
+{
+	char number[10];
+
+
+	printf("숫자를 입력하시오:\n");
+	gets(number);
+	printf("\n");
+
+	asciiToInteger(number);
+
+	printf("\n");
+
+	return 0;
+}
+
+void asciiToInteger(char *str)
+{
+
+    int result = 0;
+	int i=0;
+
+
+        if (*str >= '0' && *str <= '9')
+		{ 
+			
+
+            result =*str - '0';
+
+        }        
+
+
+	for(i=0;i<result;i++)
+	{
+		printf("%d",result);
+	}
+
+}
